44ac9c12dc00beb4326db175cdfe84442b090c4a
Eric Seidel <github@eseidel.com>
2022-12-04 08:55:05 -0800

    Add generation of local_server.dart as well
    I think generation is done!?  Now time for `shorebird run`.

---
+++ packages/shorebird/lib/src/cli/generate/generate_server.dart
@@ -0,0 +1,27 @@
+import 'package:code_builder/code_builder.dart';
+
+// From the point of view of the developer, there is no server.
+// But when we run the code locally we need some main entrypoint with
+// which to launch the server process.  Generate one here.
+Library generateServer() {
+  var library = LibraryBuilder();
+  library.body.add(Code('''
+import 'dart:io';
+
+import 'package:shorebird/datastore.dart';
+import 'package:shorebird/server.dart';
+
+import 'handlers.dart';
+import 'storable.dart';
+
+void main() async {
+  await DataStore.initSingleton(DataStoreLocal(classInfoMap, 'db.json'));
+
+  var server = Server();
+  await server.serve(allHandlers, InternetAddress.anyIPv4, 3000);
+  // ignore: avoid_print
+  print("Serving at http://\${server.address.host}:\${server.port}");
+}
+'''));
+  return library.build();
+}
