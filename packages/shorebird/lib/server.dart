38ce3f75cf6088c2b5acf595e7d056b8875141d0
Eric Seidel <github@eseidel.com>
2022-12-20 14:08:02 -0800

    Add an exception handler as middlware
    I'm not using the custom exception type anywhere yet
    but this does at least print the full exception/stacktrace when an exception happens which should help with debugging.

--- packages/shorebird/lib/server.dart
+++ packages/shorebird/lib/server.dart
@@ -8,6 +8,48 @@ import 'package:shelf_router/shelf_router.dart' as shelf_router;
 
 import 'handler.dart';
 
+/// An exception that can be thrown by the user's code and will be caught by the
+/// exception handler middleware.  Subclass this to create your own exceptions.
+/// All other exceptions will be caught and a generic 500 will be returned.
+class ShorebirdException implements Exception {
+  final String message;
+  final int status;
+
+  const ShorebirdException(this.message, {this.status = 500});
+
+  @override
+  String toString() => 'ShorebirdException: $message';
+}
+
+// Eventually this needs to be more dynamic and detect based on the request
+// type what sort of response to return.
+// Shorebird of course can be opinionated, as this is a detail that the
+// developers won't see.
+shelf.Middleware exceptionHandler() {
+  return (shelf.Handler handler) {
+    return (shelf.Request request) {
+      return Future.sync(() => handler(request))
+          .then((response) => response)
+          .catchError((Object error, StackTrace stackTrace) {
+        print("Error: $error, $stackTrace");
+        // Make sure this is an exception which is OK to expose to the user.
+        // If it is not, return something generic, like a 500.
+        // If it is, return the error message formatted in a way
+        // that the client can understand (ideally based on request headers).
+        if (error is! ShorebirdException) {
+          return shelf.Response.internalServerError(
+              body: 'Internal Server Error');
+        }
+        return shelf.Response(
+          error.status,
+          body: {'message': error.message},
+          headers: {HttpHeaders.contentTypeHeader: 'application/json'},
+        );
+      });
+    };
+  };
+}
+
 /// Used by Shorebird codegen handle centralized server logic.
 class Server {
   late final HttpServer server;
@@ -36,6 +78,7 @@ class Server {
     final handler = const shelf.Pipeline()
         .addMiddleware(shelf.logRequests())
         .addMiddleware(corsHeaders())
+        .addMiddleware(exceptionHandler())
         .addHandler(router);
     server = await shelf_io.serve(handler, InternetAddress.anyIPv4, port);
 
