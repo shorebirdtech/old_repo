bf18594043f2d1d59c881a4baca4fbdfc6a7f3ec
Eric Seidel <github@eseidel.com>
2023-02-24 19:00:44 -0800

    Add updater_demo example
    Currently just the counter app, but will turn it into the updater demo next.

---
+++ examples/updater_demo/README.md
@@ -0,0 +1,3 @@
+# updater_demo
+
+Attempting to wire in the updater library into a flutter app.
---
+++ examples/updater_demo/android/app/src/main/kotlin/com/example/updater_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.updater_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
