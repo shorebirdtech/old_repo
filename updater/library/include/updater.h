a36b4b809fcf0d97626c8d9f37a9bc70538e5f5f
Eric Seidel <eric@shorebird.dev>
2023-02-27 15:05:11 -0800

    Update updater.h
    Add extern "C" wrappers to allow including correctly in C++ programs (like the Flutter engine).

--- updater/library/include/updater.h
+++ updater/library/include/updater.h
@@ -1,10 +1,20 @@
 #ifndef updater_h
 #define updater_h
 
-char *active_version(const char *client_id, const char *cache_dir);
-char *active_path(const char *client_id, const char *cache_dir);
-bool check_for_update(const char *client_id, const char *cache_dir);
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
-void free_string(char *str);
+    char *active_version(const char *client_id, const char *cache_dir);
+    char *active_path(const char *client_id, const char *cache_dir);
+    bool check_for_update(const char *client_id, const char *cache_dir);
+    void update(const char *client_id, const char *cache_dir);
+
+    void free_string(char *str);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif /* updater_h */
