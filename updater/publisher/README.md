88580ef05d8af8d4854208c34c3ee0bd57c8d200
Eric Seidel <eric@shorebird.dev>
2023-02-27 17:27:56 -0800

    Add "publisher" command
    This really belongs as part of `shorebird`, but I need to delete a bunch of dead code from `shorebird` first, so for now just making this a separate package/command.

---
+++ updater/publisher/README.md
@@ -0,0 +1,1 @@
+This is just a placeholder and should move into the `shorebird` command.
---
+++ updater/publisher/bin/publisher.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+import 'package:args/command_runner.dart';
+import 'package:http/http.dart' as http;
+
+void main(List<String> args) async {
+  final runner = CommandRunner<void>('shorebird', 'Shorebird CLI')
+    ..addCommand(Publish());
+  await runner.run(args);
+}
+
+Future<void> uploadToDeployServer({
+  required String path,
+  required Uri deployUrl,
+}) async {
+  var request = http.MultipartRequest('POST', deployUrl);
+  var file = await http.MultipartFile.fromPath('file', path);
+  request.files.add(file);
+  var response = await request.send();
+  if (response.statusCode == 200) {
+    print('Deployed successful.');
+  } else {
+    print('Deploy failed: ${response.statusCode} ${response.reasonPhrase}');
+  }
+  exit(0);
+}
+
+class Publish extends Command<void> {
+  Publish();
+
+  @override
+  final name = 'publish';
+
+  @override
+  final description = 'Publish an update.';
+
+  @override
+  void run() async {
+    if (argResults!.rest.isEmpty) {
+      print('No file specified.');
+      return;
+    }
+    // Which file are we sending to the server?
+    var path = argResults!.rest[0];
+
+    final deployUri = Uri.parse('http://localhost:8080/deploy');
+    await uploadToDeployServer(path: path, deployUrl: deployUri);
+
+    // This exit should not be needed, but otherwise it just hangs forever.
+    exit(0);
+  }
+}
